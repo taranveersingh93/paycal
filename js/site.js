a4e2565228e25a27a99dd507bbcf8fb95928ea15
Taranveer Singh <taranveersingh93@gmail.com>
2023-10-23 18:06:52 -0400

    Add downpayment option

--- js/site.js
+++ js/site.js
@@ -6,9 +6,8 @@ const processForm = (event) => {
     formEntries = convertToNumbers(formEntries);
     const formOk = validateEntries(formEntries);
     let summary = calculateSummary(formEntries);
-    let payments = calculatePayments(formEntries);
-    console.log(payments);
-    // displayPayments(payments);
+    let payments = calculatePayments(summary);
+    displayPayments(payments);
 
     if (formOk && formEntries.targetPayment) {
        displayDownPaymentSummary(summary);
@@ -17,11 +16,10 @@ const processForm = (event) => {
     }
 }
 
-const calculatePayments = entries => {
-    const {loanAmount, termLength, interestRate, targetPayment} = entries;
-    let remainingPrincipal = loanAmount;
+const calculatePayments = summary => {
+    const {monthlyPayment, interestRate, totalCost, termLength, principal} = summary;
+    let remainingPrincipal = principal;
     let totalInterest = 0;
-    const monthlyPayment = ((loanAmount * (interestRate/1200)) / (1 - (1 + interestRate/1200) ** (-1*termLength))).toFixed(2);
     const tableData = [];
 
     for (let i = 0; i < termLength; i++) {
@@ -49,6 +47,41 @@ const calculatePayments = entries => {
     return tableData;
 }
 
+const displayPayments = payments => {
+    const table = document.getElementById('paymentsTable');
+
+    const tableHeading = table.querySelector('thead');
+    const headerTemplate = document.getElementById('tableHeadingTemplate');
+    const headerContent = headerTemplate.content.cloneNode(true);
+    tableHeading.innerHTML = '';
+    tableHeading.appendChild(headerContent);
+
+    const tableBody = table.querySelector('tbody');
+    const rowTemplate = document.getElementById('tableRowTemplate');
+    tableBody.innerHTML = '';
+    for (let i = 0; i < payments.length; i++) {
+        const paymentObj = payments[i];
+        const {month, payment, principal, interest, totalInterest, balance} = paymentObj;
+        const rowContent  = rowTemplate.content.cloneNode(true);
+        const monthElement = rowContent.querySelector('.table-month');
+        const paymentElement = rowContent.querySelector('.table-payment');
+        const principalElement = rowContent.querySelector('.table-principal');
+        const totalInterestElement = rowContent.querySelector('.table-total-interest');
+        const interestElement = rowContent.querySelector('.table-interest');
+        const balanceElement = rowContent.querySelector('.table-balance');
+        monthElement.innerText = month;
+        paymentElement.innerText = payment.toFixed(2);
+        principalElement.innerText = principal.toFixed(2);
+        interestElement.innerText = interest.toFixed(2);
+        totalInterest.innerText = totalInterest.toFixed(2);
+        balanceElement.innerText = balance.toFixed(2);
+        totalInterestElement.innerText = totalInterest.toFixed(2);
+
+        tableBody.appendChild(rowContent);
+    }
+
+}
+
 const calculateSummary = entries => {
     const {loanAmount, termLength, interestRate, targetPayment} = entries;
     const summary = {
@@ -56,16 +89,50 @@ const calculateSummary = entries => {
         principal: 0, 
         interest: 0, 
         totalCost: 0, 
-        downPayment: 0
+        downPayment: 0,
+        termLength,
+        interestRate
     };
 
+    const minimumPayment = ((loanAmount * (interestRate/1200)) / (1 - (1 + interestRate/1200) ** (-1*termLength))).toFixed(2);
+
     if (!targetPayment) {
-        summary.monthlyPayment = ((loanAmount * (interestRate/1200)) / (1 - (1 + interestRate/1200) ** (-1*termLength))).toFixed(2);
+        summary.monthlyPayment = minimumPayment;
         summary.principal = loanAmount;
         summary.totalCost = (summary.monthlyPayment * termLength);
         summary.interest = (summary.totalCost - summary.principal);
+    } else if (minimumPayment > targetPayment) {
+        summary.monthlyPayment = targetPayment;
+        const minimumCost = minimumPayment * termLength;
+        summary.totalCost = targetPayment * termLength;
+        summary.downPayment = minimumCost - summary.totalCost;
+        summary.principal = loanAmount - summary.downPayment;
+        summary.interest = summary.totalCost - summary.principal;
     }
     return summary; 
+}
+
+const displayDownPaymentSummary = summary => {
+    const {monthlyPayment, principal, interest, totalCost, downPayment} = summary;
+    const detailsSection = document.getElementById('detailsSection');
+    detailsSection.innerHTML = '';
+    const detailsTemplate = document.getElementById('detailsTemplate');
+    const detailsElement = detailsTemplate.content.cloneNode(true);
+    const headline = detailsElement.querySelector('.payment-headline');
+    const headAmount = detailsElement.querySelector('.payment-headline-amount');
+    const principalAmount = detailsElement.getElementById('principalAmount');
+    const interestAmount = detailsElement.getElementById('interestAmount');
+    const costAmount = detailsElement.getElementById('costAmount');
+    const finalAmount = detailsElement.getElementById('finalAmount');
+    const finalText = detailsElement.getElementById('finalText');
+    headline.innerText = "Your Downpayment:"
+    headAmount.innerText = `$${downPayment.toLocaleString()}`;
+    principalAmount.innerText = `$${principal.toLocaleString()}`;
+    interestAmount.innerText = `$${interest.toLocaleString()}`;
+    costAmount.innerText = `$${totalCost.toLocaleString()}`;
+    finalAmount.innerText = `$${monthlyPayment.toLocaleString()}`;
+    finalText.innerHTML = '<strong>Monthly Payment</strong>'
+    detailsSection.appendChild(detailsElement);
 }
 
 const displaySummary = summary => {
